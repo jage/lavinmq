886ae9152e7bcb33f58c58008882fe69d720e086
Johan Eckerström <johan@duh.se>
2026-04-16 17:06:31 +0200

    Move y-axis to right side, align chart and legend to the left

--- static/js/chart-uplot.js
+++ static/js/chart-uplot.js
@@ -148,6 +148,7 @@ function initChart (handle, filled) {
         values: (u, vals) => vals.map(v => new Date(v * 1000).toLocaleTimeString('en-GB'))
       },
       {
+        side: 1,
         stroke: axisColor,
         grid: { stroke: gridColor, dash: [2, 4] },
         ticks: { stroke: gridColor },
